57941e9fc0d5bf88953e989c103267e57677076c
arifhasnatnstucsteonGit <arif.hasnat@dreamonline.co.jp>
2021-11-24 16:05:43 +0600

    collection view cell

---
+++ IOS Learnings/youtubePlaylist/views/cells/YoutubePlaylistCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  YoutubePlaylistCollectionViewCell.swift
+//  IOS Learnings
+//
+//  Created by DREAMONLINE on 24/11/21.
+//
+
+import UIKit
+
+class YoutubePlaylistCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var videoPreview: UIImageView!
+    
+}
---
+++ IOS Learnings/youtubePlaylist/views/controllers/YoutubePlaylistViewController.swift
@@ -0,0 +1,77 @@
+//
+//  YoutubePlaylistViewController.swift
+//  IOS Learnings
+//
+//  Created by DREAMONLINE on 24/11/21.
+//
+
+import UIKit
+
+class YoutubePlaylistViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource{
+
+   
+    var thumbnails = [String]()
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.async {
+            self.getYoutubePlaylistData()
+        }
+        
+
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! YoutubePlaylistCollectionViewCell
+        //cell.videoPreview.image
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return thumbnails.count
+    }
+    
+
+    
+    func getYoutubePlaylistData(){
+        let urlComponent = NSURLComponents(string: "https://www.googleapis.com/youtube/v3/playlistItems")!
+        urlComponent.queryItems = [
+            URLQueryItem(name: "part", value: "snippet"),
+            URLQueryItem(name: "maxResults", value: "25"),
+            URLQueryItem(name: "playlistId", value: "PLs6YIr-PdysBBJKQEKd1FSMFF3_g6_ien"),
+            URLQueryItem(name: "pageToken", value: ""),
+            URLQueryItem(name: "key", value: "AIzaSyBbx1tWZ_j17HBq7y84NF_B_QQQGxNSz5o")
+        ]
+        
+        URLSession.shared.dataTask(with: urlComponent.url!, completionHandler: { data, response, error -> Void in
+            do {
+                let jsonDecoder = JSONDecoder()
+                let responseModel = try jsonDecoder.decode(YoutubePlaylistModel.self, from: data!)
+                //print(responseModel.items![0].snippet?.resourceId?.videoId)
+               // print(responseModel.items![0].snippet?.thumbnails?.standard?.url)
+                var items = [Items]()
+                items = responseModel.items!
+                
+                for item in items{
+                    self.thumbnails.append((item.snippet?.thumbnails?.standard?.url!)!)
+                }
+                print(self.thumbnails)
+                
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+              
+                
+                //self.thumbnails.append((responseModel.items![0].snippet?.thumbnails?.standard?.url!)!)
+            } catch {
+                print("JSON Serialization error")
+            }
+        }).resume()
+    }
+    
+}
+
